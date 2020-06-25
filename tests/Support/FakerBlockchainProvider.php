1a1a1042429bc4cb85fa56e5d5bf3f8f949f65c6
Carl Olsen <unstoppablecarlolsen@gmail.com>
2020-06-25 10:33:24 -0400

    cleanup

--- tests/Support/FakerBlockchainProvider.php
+++ tests/Support/FakerBlockchainProvider.php
@@ -19,17 +19,21 @@ class FakerBlockchainProvider extends Base
     public function uint256()
     {
         $max = '115792089237316195423570985008687907853269984665640564039457584007913129639935';
-        $chunks = chunk_split($max, 8, ',');
+
+        return $this->bigNumberBetween(0, $max);
+    }
+
+    public function bigNumberBetween(string $min, string $max, int $chunkSize = 8)
+    {
+        $chunks = chunk_split($max, $chunkSize, ',');
         $chunks = trim($chunks, ',');
         $chunks = explode(',', $chunks);
         $output = '';
         foreach ($chunks as $chunk) {
-            $number = $this->generator->numberBetween(0, $chunk);
+            $number = $this->generator->numberBetween($min, $chunk);
 
-            $number = str_pad($number, 8, 0, STR_PAD_LEFT);
+            $number = str_pad($number, $chunkSize, 0, STR_PAD_LEFT);
             $output .= $number;
         }
-
-        return $output;
     }
 }
